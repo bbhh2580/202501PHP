af5dd479b12edb902f40bd2508ce1110aa2f6a15
Hana <fweriuhb@gmail.com>
2025-02-03 17:09:42 +0900

    Update db.php

--- product/database/db.php
+++ product/database/db.php
@@ -5,8 +5,25 @@
 require_once '../config/config.php';
 
 class Database {
-    private $conn; // database connection
-    public function __construct() {
-        $this->conn = Database::getInstance()->getConnection();
+    private static $instance = null; // 保存类实例的静态变量
+    private $conn; // 数据库连接
+
+    // 构造函数, privately, 防止直接创造对象
+    private function __construct() {
+        global $db;
+        $this->conn = $db;
+    }
+
+    // 获取类实例的公共静态方法
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    // 获取数据库连接的公共方法
+    public function getConnection() {
+        return $this->conn;
     }
 }
